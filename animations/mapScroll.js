d9d8907807c9c1c4cd1fa6345ea48d60bb38f7dd
lauhengyi <77540391+lauhengyi@users.noreply.github.com>
2022-10-04 18:43:27 +0800

    added unnecessary gsap set

--- animations/mapScroll.js
+++ animations/mapScroll.js
@@ -2,7 +2,6 @@ import gsap from 'gsap';
 import styles from '../styles/Home.module.css';
 
 export default function mapScroll(q) {
-  gsap.set(q(), { strokeDasharray: length });
   return gsap
     .timeline({ default: { ease: 'power2.inOut' } })
     .to(q(`.${styles.mapLine}`), { strokeDashoffset: 0, duration: 5 })
