cf6221b5ca0752b2193c791adc89fd19b9cae8f4
lauhengyi <lauhengyi@gmail.com>
2022-10-17 18:02:52 +0800

    Added text collapsing animation

--- animations/catelogTransition.js
+++ animations/catelogTransition.js
@@ -4,6 +4,26 @@ import styles from '../styles/Home.module.css';
 export default function catelogTransition(q) {
   const amountToSlide = 250;
   const slideDuration = 20;
+
+  const textCollapsing = gsap
+    .timeline()
+    .to(
+      q(`.${styles.catelogText} p:not(:nth-of-type(1)):not(.${styles.anchor})`),
+      {
+        width: '0',
+        duration: 3,
+        ease: 'back.in',
+      },
+    )
+    .to(
+      q(`.${styles.catelogText} p:nth-of-type(1)`),
+      {
+        paddingRight: '3vh',
+        duration: 3,
+      },
+      '<',
+    );
+
   // Add all animations that is not the sliding animation to the timeline
   const mainMovements = gsap
     .timeline()
@@ -17,7 +37,8 @@ export default function catelogTransition(q) {
       },
       '<+=0.3',
     )
-    .to(q(`.${styles.catelogRowGroup}`), { rotate: 0, duration: 3 }, '+=2')
+    .add(textCollapsing)
+    .to(q(`.${styles.catelogRowGroup}`), { rotate: 0, duration: 3 }, '<+2')
     .to(
       q(`.${styles.catelogTransitionBackground}`),
       {
--- components/CatalogTransitionScene.js
+++ components/CatalogTransitionScene.js
@@ -4,20 +4,30 @@ import CatalogVector from './CatalogVector';
 
 export default function CatalogTransitionScene() {
   const numberOfItemsPerRow = 60;
-  const merchantText = 'MERCHANT';
-  const customerText = 'CUSTOMER';
   return (
     <div className={styles.transitionContainer}>
       <span className={styles.catelogTransitionBackground} />
-      <div className={`${styles.catelogText} ${styles.merchant}`}>
-        {[...merchantText].map((l, i) => (
-          <p key={i}>{l}</p>
-        ))}
-      </div>
       <div className={`${styles.catelogText} ${styles.customer}`}>
-        {[...customerText].map((l, i) => (
-          <p key={i}>{l}</p>
-        ))}
+        <p>C</p>
+        <p>C</p>
+        <p>U</p>
+        <p>S</p>
+        <p>T</p>
+        <p>O</p>
+        <p>M</p>
+        <p>E</p>
+        <p className={styles.anchor}>R</p>
+      </div>
+      <div className={`${styles.catelogText} ${styles.merchant}`}>
+        <p className={styles.anchor}>M</p>
+        <p>M</p>
+        <p>E</p>
+        <p>R</p>
+        <p>C</p>
+        <p>H</p>
+        <p>A</p>
+        <p>N</p>
+        <p>T</p>
       </div>
       <div className={styles.catelogRowGroup}>
         <div className={styles.catelogRow}>
