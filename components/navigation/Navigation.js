90e1e378f61eb55e010d497f0b27fe2813717add
lauhengyi <77540391+lauhengyi@users.noreply.github.com>
2022-11-07 19:01:42 +0800

    Added nav button animation and fixed its position

--- components/navigation/Navigation.js
+++ components/navigation/Navigation.js
@@ -9,6 +9,7 @@ export default function Navigation() {
 
   const el = useRef();
   const menuOpenAnimation = useRef();
+  const menuButtonAnimation = useRef();
   let q = gsap.utils.selector(el);
 
   useEffect(() => {
@@ -20,12 +21,34 @@ export default function Navigation() {
         { '--clip': '0vh' },
         { '--clip': '150vh', duration: 0.7 },
       );
+
+    menuButtonAnimation.current = gsap
+      .timeline({ paused: true })
+      .fromTo(
+        q(`.${styles.menuButtonLine}:nth-of-type(1)`),
+        { rotate: '0deg', x: '0rem' },
+        { rotate: '45deg', x: '0.25rem', duration: 0.3 },
+      )
+      .fromTo(
+        q(`.${styles.menuButtonLine}:nth-of-type(3)`),
+        { rotate: '0deg', x: '0rem' },
+        { rotate: '-45deg', x: '0.25rem', duration: 0.3 },
+        '<',
+      )
+      .fromTo(
+        q(`.${styles.menuButtonLine}:nth-of-type(2)`),
+        { opacity: 1, x: '0rem' },
+        { opacity: 0, x: '-1rem', duration: 0.3 },
+        '<',
+      );
   });
   const toggleMenu = () => {
     if (isMenuOpen.current) {
       menuOpenAnimation.current.reverse();
+      menuButtonAnimation.current.reverse();
     } else {
       menuOpenAnimation.current.play();
+      menuButtonAnimation.current.play();
     }
     isMenuOpen.current = !isMenuOpen.current;
   };
