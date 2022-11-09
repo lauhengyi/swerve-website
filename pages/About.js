a8c3bc23d65487188e3c6dcc0bf9f2de956e9f68
lauhengyi <77540391+lauhengyi@users.noreply.github.com>
2022-11-09 22:15:36 +0800

    Added team member portion

--- pages/About.js
+++ pages/About.js
@@ -3,6 +3,7 @@ import AppContext from '../components/AppContext';
 import styles from '../styles/About.module.css';
 import aboutText from '../texts/aboutText';
 import navbarText from '../texts/navbarText';
+import Image from 'next/image';
 import Head from 'next/head';
 import Navigation from '../components/navigation/Navigation';
 
@@ -33,6 +34,46 @@ export default function About() {
             <div className={styles.orbsBox}>
               <span className={styles.orb} />
               <span className={styles.orb} />
+            </div>
+          </section>
+          <section className={styles.sectionContainer}>
+            <div className={styles.textBox}>
+              <h1 className={styles.header}>
+                {aboutText.ourTeam.header[lang]}
+                <span className="accentText">
+                  {aboutText.ourTeam.headerAccent[lang]}
+                </span>
+              </h1>
+              <div className={styles.memberContainer}>
+                <div className={styles.headshotContainer}>
+                  <Image
+                    className={styles.headshot}
+                    height={300}
+                    width={300}
+                    src={'/headshots/hengyi.jpg'}
+                    alt={"Lau Heng Yi's Headshot"}
+                  />
+                </div>
+                <div className={styles.memberHeaderContainer}>
+                  <h2 className={styles.memberName}>
+                    {aboutText.ourTeam.hengYi[lang]}
+                  </h2>
+                  <h3 className={styles.memberTitle}>
+                    {aboutText.ourTeam.hengYiTitle[lang]}
+                  </h3>
+                </div>
+                <div className={styles.memberDescription}>
+                  <p className={styles.text}>
+                    {aboutText.ourTeam.hengYiText1[lang]}
+                  </p>
+                  <p className={styles.text}>
+                    {aboutText.ourTeam.hengYiText2[lang]}
+                  </p>
+                  <p className={styles.text}>
+                    {aboutText.ourTeam.hengYiText3[lang]}
+                  </p>
+                </div>
+              </div>
             </div>
           </section>
         </article>
