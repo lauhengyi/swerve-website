537ae4a4fe3d9804ddb733ec51b1e97f390caf39
lauhengyi <77540391+lauhengyi@users.noreply.github.com>
2022-11-09 22:08:13 +0800

    Completed about text

--- texts/aboutText.js
+++ texts/aboutText.js
@@ -7,6 +7,8 @@ const aboutText = {
         into the online shopping market. Swerve aims to do this by providing shop owners with a more 
         seamless and accessible way to do digital advertising, as well as to make it more convenient 
         for consumers to find the products they want from physical stores.`,
+      `寻机的企业理念是要帮助各个城市的商家提高销售量，通过网上购物让顾客对您的商店有更好的认知，从而让商家有更
+      好的竞争优势。寻机即将提供一个即平稳又通畅的平台让你们在网上推销。这可以让顾客更方便的接触到你们所推销的产品。`,
     ],
     text2: [
       `Our concept involves creating a platform for merchants to upload a digital catalogue of 
@@ -14,10 +16,62 @@ const aboutText = {
         products listed on the catalogues using our search engine, to find the best deals, or to 
         find the store nearest to them. Consumers can even follow their favourite stores to stay 
         up to date with new product releases and promotions.`,
+      `寻机会在网上设计一个平台让商家展示他们的产品，然而也让商家们推销优惠。顾客便可以在网上搜索商店推
+        销的产品，从而找到最好的优惠，也可以在顾客附近的提示找到离他们最近的商店。顾客也可以订阅他们所喜欢
+        的商店并且看到商店最新推销的产品与优惠。`,
     ],
     text3: [
       `With Swerve, consumers will be able to browse products from physical stores at their fingertips,
         alongside e-commerce options, giving physical stores a fair playing ground in today's age.`,
+      `寻机让顾客可以在网上搜索商品，因此让实体商店与网上商店有了更公平的竞争。`,
+    ],
+  },
+  ourTeam: {
+    header: ['Introducing ', '我们的'],
+    headerAccent: ['our Team!', '团队!'],
+
+    // Lau Heng Yi
+    hengYi: ['Lau Heng Yi', '刘恒奕'],
+    hengYiTitle: ['Founder', '寻机的创始人'],
+    hengYiText1: [
+      `Since young, Heng Yi had the passion to create and use his talents to make the world a better place.`,
+      `恒奕在很小的时候就热爱创造，凭着他对世界的认知与天赋，创造了许多物品让这世界变得更美好。`,
+    ],
+    hengYiText2: [
+      `Being a self-directed learner and a self-starter, Heng Yi has attained a deep understanding of Computer 
+    Science concepts well beyond his peers and initiated many personal projects to refine his skills, such as building
+     the 21Chess Mobile App and designing a Financial Market Trading Algorithm.`,
+      `恒奕是个喜欢自主学习的人。他通过多年的学习，计算机科学水平提升了许多。他主动参与了许多计算机科学类的课程来提高水平。
+      例如他使用了计算机科学知识写了代码程序，创作了一个下棋软件。`,
+    ],
+    hengYiText3: [
+      `He first came up with the idea of Swerve while pondering how he could help traditional brick-and-mortar 
+      stores remain relevant in this era of e-commerce. He saw the perfect opportunity to use his software 
+      engineering skills to make a positive impact in this aspect and began building the Swerve App.`,
+      `寻机的启发来源于一个困难。一个困扰着许多商家的问题。随着科技与电脑越来越发达，网上购物变得平凡，许多实体商店逐渐被淘汰。
+      此时，我们想到了办法帮助商家仍然能站有一席之地。那就是把科技网络融入与商店的销售过程。`,
+    ],
+
+    // Roydon Tay
+    roydon: ['Roydon Tay', '陈瑞东'],
+    roydonTitle: ['Co-Founder', '副董事长'],
+    roydonText1: [
+      `An avid learner, Roydon never misses any opportunity to expand his knowledge.`,
+      `楷颖非常好学，并且喜欢活学活用。他可说是名副其实的活到老学到老。`,
+    ],
+    roydonText2: [
+      `He explores his interest in data science with personal projects, handling data and building his 
+      own machine learning models.`,
+      `他对数据科学有着非比寻常的兴趣。因此他安排了许多有关数据科学的项目来提高他的水平。例如数据管理，数据分析等等。`,
+    ],
+    roydonText3: [
+      `Besides learning technical skills, he also interests himself in statistics, psychology and philosophy. 
+      His extensive knowledge in various fields allows him to come up with unique insights and ideas for Swerve.`,
+      '除此之外，楷颖也对心理学与哲学有着深厚的了解。有了更广泛的知识，他可以在寻机的发展道路上提供更多的建议与启发。',
+    ],
+    roydonText4: [
+      `His adventurous spirit and curiosity continuously drives him to explore new possibilities and innovations.`,
+      `楷颖的上进心与好奇心的驱使下让他对创新有了更强的追求。`,
     ],
   },
 };
