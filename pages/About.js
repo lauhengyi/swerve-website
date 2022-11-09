55f3b0eb487907a6a144bab88c5464d031a57954
lauhengyi <77540391+lauhengyi@users.noreply.github.com>
2022-11-09 16:54:44 +0800

    Done about us section of about page

--- pages/About.js
+++ pages/About.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useContext } from 'react';
 import AppContext from '../components/AppContext';
 import styles from '../styles/About.module.css';
 import aboutText from '../texts/aboutText';
@@ -18,16 +18,22 @@ export default function About() {
       <main className={styles.main}>
         <Navigation />
         <article className={styles.innerContainer}>
-          <section className={styles.aboutUs}>
-            <h1 className={styles.header}>
-              {aboutText.aboutUs.header[lang]}
-              <span className="accentText">
-                {aboutText.aboutUs.headerAccent[lang]}
-              </span>
-            </h1>
-            <p className={styles.text}>{aboutText.aboutUs.text1[lang]}</p>
-            <p className={styles.text}>{aboutText.aboutUs.text2[lang]}</p>
-            <p className={styles.text}>{aboutText.aboutUs.text3[lang]}</p>
+          <section className={styles.sectionContainer}>
+            <div className={styles.textBox}>
+              <h1 className={styles.header}>
+                {aboutText.aboutUs.header[lang]}
+                <span className="accentText">
+                  {aboutText.aboutUs.headerAccent[lang]}
+                </span>
+              </h1>
+              <p className={styles.text}>{aboutText.aboutUs.text1[lang]}</p>
+              <p className={styles.text}>{aboutText.aboutUs.text2[lang]}</p>
+              <p className={styles.text}>{aboutText.aboutUs.text3[lang]}</p>
+            </div>
+            <div className={styles.orbsBox}>
+              <span className={styles.orb} />
+              <span className={styles.orb} />
+            </div>
           </section>
         </article>
       </main>
