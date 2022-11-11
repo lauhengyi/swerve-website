db1aaa98b704da41904582432976300ab49b2618
lauhengyi <77540391+lauhengyi@users.noreply.github.com>
2022-11-11 20:07:39 +0800

    Transfer to home

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,16 +1,28 @@
+import React, { useContext } from 'react';
+import AppContext from '../components/AppContext';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Contact.module.css';
 import Navigation from '../components/navigation/Navigation';
+import navbarText from '../texts/navbarText';
+import contactText from '../texts/contactText';
 
 export default function Contact() {
+  const { lang } = useContext(AppContext);
   return (
     <div>
       <Head>
-        <title>Contact</title>
+        <title>{navbarText.contact[lang]}</title>
         <link rel="icon" href="/swerve_icon.png" />
       </Head>
-      <main className={styles.main}>
-        <Navigation />
+      <main className="main">
+        {/* <Navigation /> */}
+        {/* <article className="innerContainer">
+          <section className={`${styles.sectionContainer} standardWidth`} />
+          <h1 className="header">
+            {contactText.header[lang]}
+            <span className="accentText">{contactText.headerAccent[lang]}</span>
+          </h1>
+        </article> */}
       </main>
     </div>
   );
--- texts/contactText.js
+++ texts/contactText.js
@@ -1,6 +1,6 @@
 const contactText = {
-  header: ['Get in touch', '联系'],
-  headerAccent: ['Us', '我们'],
+  header: ['Get in touch with ', '联系'],
+  headerAccent: ['us', '我们'],
   phone: ['Phone', '电话'],
   phoneNumber: '+65 9132 9449',
   generalEmail: ['General Enquiries', '一般查询'],
