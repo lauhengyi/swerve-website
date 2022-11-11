709be4145d533d74dde590a3b1a465c93d565159
lauhengyi <77540391+lauhengyi@users.noreply.github.com>
2022-11-11 10:37:01 +0800

    Added contact text

---
+++ texts/contactText.js
@@ -0,0 +1,12 @@
+const contactText = {
+  header: ['Get in touch', '联系'],
+  headerAccent: ['Us', '我们'],
+  phone: ['Phone', '电话'],
+  phoneNumber: '+65 9132 9449',
+  generalEmail: ['General Enquiries', '一般查询'],
+  generalEmailAddress: 'swerveappteam@gmail.com',
+  supportEmail: ['Support', '支持'],
+  supportEmailAddress: 'swerveapphelp@gmail.com',
+};
+
+export default contactText;
