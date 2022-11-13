66b031c773bd2095dc0c11f15a580ff3756aaff3
lauhengyi <77540391+lauhengyi@users.noreply.github.com>
2022-11-13 22:22:07 +0800

    Contact page done

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import Image from 'next/image';
 import AppContext from '../components/AppContext';
 import Head from 'next/head';
 import styles from '../styles/Contact.module.css';
@@ -15,14 +16,55 @@ export default function Contact() {
         <link rel="icon" href="/swerve_icon.png" />
       </Head>
       <main className="main">
-        {/* <Navigation /> */}
-        {/* <article className="innerContainer">
-          <section className={`${styles.sectionContainer} standardWidth`} />
-          <h1 className="header">
-            {contactText.header[lang]}
-            <span className="accentText">{contactText.headerAccent[lang]}</span>
-          </h1>
-        </article> */}
+        <Navigation />
+        <article className="innerContainer">
+          <section className={`${styles.sectionContainer} standardWidth`}>
+            <h1 className="header">
+              {contactText.header[lang]}
+              <span className="accentText">
+                {contactText.headerAccent[lang]}
+              </span>
+            </h1>
+            <div className={styles.contactDetails}>
+              <div className={styles.contactContainer}>
+                <Image
+                  src="/contactIcons/phone.svg"
+                  alt="Phone"
+                  height={50}
+                  width={50}
+                />
+                <h2 className={styles.contactTitle}>
+                  {contactText.phone[lang]}
+                </h2>
+                <p className="text">{contactText.phoneNumber}</p>
+              </div>
+              <div className={styles.contactContainer}>
+                <Image
+                  src="/contactIcons/email.svg"
+                  alt="Email"
+                  height={50}
+                  width={50}
+                />
+                <h2 className={styles.contactTitle}>
+                  {contactText.generalEmail[lang]}
+                </h2>
+                <p className="text">{contactText.generalEmailAddress}</p>
+              </div>
+              <div className={styles.contactContainer}>
+                <Image
+                  src="/contactIcons/question.svg"
+                  alt="Support"
+                  height={50}
+                  width={50}
+                />
+                <h2 className={styles.contactTitle}>
+                  {contactText.supportEmail[lang]}
+                </h2>
+                <p className="text">{contactText.supportEmailAddress}</p>
+              </div>
+            </div>
+          </section>
+        </article>
       </main>
     </div>
   );
--- texts/contactText.js
+++ texts/contactText.js
@@ -2,7 +2,7 @@ const contactText = {
   header: ['Get in touch with ', '联系'],
   headerAccent: ['us', '我们'],
   phone: ['Phone', '电话'],
-  phoneNumber: '+65 9132 9449',
+  phoneNumber: '+65 8113 7678',
   generalEmail: ['General Enquiries', '一般查询'],
   generalEmailAddress: 'swerveappteam@gmail.com',
   supportEmail: ['Support', '支持'],
