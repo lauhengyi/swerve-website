f2c40f38d72e915e20ea80e1607c07d503805ec9
lauhengyi <77540391+lauhengyi@users.noreply.github.com>
2022-11-14 08:32:25 +0800

    Added download text

---
+++ texts/downloadText.js
@@ -0,0 +1,11 @@
+const downloadText = {
+  header: ['The app is still in developemnt', '应用程序仍在开发中'],
+  headerAccent: ['Get notified about its release', '获取有关其发布的通知'],
+  email: ['Enter your email', '输入您的电子邮件'],
+  caption: [
+    'We will send you updates about the app',
+    '我们将向您发送有关应用程序的更新',
+  ],
+};
+
+export default downloadText;
