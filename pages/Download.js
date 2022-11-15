71664144f90ba7d1f2657af396ccd8e2f6dcca6d
lauhengyi <77540391+lauhengyi@users.noreply.github.com>
2022-11-15 16:46:06 +0800

    Added submit button

--- pages/Download.js
+++ pages/Download.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react';
 import AppContext from '../components/AppContext';
 import Head from 'next/head';
+import Image from 'next/image';
 import styles from '../styles/Download.module.css';
 import Navigation from '../components/navigation/Navigation';
 import navbarText from '../texts/navbarText';
@@ -25,19 +26,28 @@ const Download = () => {
         <article className="innerContainer">
           <section className={`${styles.sectionContainer} standardWidth`}>
             <h1 className={`${styles.header} header`}>
-              {' '}
               {downloadText.header[lang]}
             </h1>
             <h2 className={`${styles.header2} accentText`}>
               {downloadText.headerAccent[lang]}
             </h2>
-            <input
-              type="email"
-              placeholder={downloadText.emailPlaceholder[lang]}
-              className={styles.input}
-              value={email}
-              onChange={handleChange}
-            />
+            <form className={styles.form}>
+              <input
+                type="email"
+                placeholder={downloadText.emailPlaceholder[lang]}
+                className={styles.input}
+                value={email}
+                onChange={handleChange}
+              />
+              <button className={styles.button} type="submit">
+                <Image
+                  src="/arrow.svg"
+                  height={100}
+                  width={100}
+                  alt="responsive"
+                />
+              </button>
+            </form>
             <h3 className={styles.caption}>{downloadText.caption[lang]}</h3>
           </section>
         </article>
