618cc0dbc83a738d9ca67a70cdbfe77d900a4c23
lauhengyi <77540391+lauhengyi@users.noreply.github.com>
2022-11-15 09:43:10 +0800

    Download screen template done

--- pages/Download.js
+++ pages/Download.js
@@ -1,16 +1,46 @@
+import React, { useContext, useState } from 'react';
+import AppContext from '../components/AppContext';
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Download.module.css';
 import Navigation from '../components/navigation/Navigation';
+import navbarText from '../texts/navbarText';
+import downloadText from '../texts/downloadText';
 
 const Download = () => {
+  const { lang } = useContext(AppContext);
+  const [email, setEmail] = useState();
+
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+  };
+
   return (
     <div>
       <Head>
-        <title>Download</title>
+        <title>{navbarText.download[lang]}</title>
         <link rel="icon" href="/swerve_icon.png" />
       </Head>
-      <main className={styles.main}>
+      <main className="main">
         <Navigation />
+        <article className="innerContainer">
+          <section className={`${styles.sectionContainer} standardWidth`}>
+            <h1 className={`${styles.header} header`}>
+              {' '}
+              {downloadText.header[lang]}
+            </h1>
+            <h2 className={`${styles.header2} accentText`}>
+              {downloadText.headerAccent[lang]}
+            </h2>
+            <input
+              type="email"
+              placeholder={downloadText.emailPlaceholder[lang]}
+              className={styles.input}
+              value={email}
+              onChange={handleChange}
+            />
+            <h3 className={styles.caption}>{downloadText.caption[lang]}</h3>
+          </section>
+        </article>
       </main>
     </div>
   );
--- texts/downloadText.js
+++ texts/downloadText.js
@@ -1,11 +1,14 @@
 const downloadText = {
-  header: ['The app is still in developemnt', '应用程序仍在开发中'],
+  header: ['The app is still in development', '应用程序仍在开发中'],
   headerAccent: ['Get notified about its release', '获取有关其发布的通知'],
-  email: ['Enter your email', '输入您的电子邮件'],
+  emailPlaceholder: ['Enter your email', '输入您的电子邮件'],
   caption: [
-    'We will send you updates about the app',
+    "We'll send you updates about the app",
     '我们将向您发送有关应用程序的更新',
   ],
+  inputError: ['Please enter a valid email', '请输入有效的电子邮件'],
+  serverError: ['Something went wrong', '出了点问题'],
+  success: ['Thank you for your interest', '感谢您的关注'],
 };
 
 export default downloadText;
